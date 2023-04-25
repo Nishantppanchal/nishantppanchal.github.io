679549a52bf46fedb784bf4e93dad786f0343542
Nishant Panchal <68797723+Nishantppanchal@users.noreply.github.com>
2023-04-25 13:35:53 +1000

    Removing google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,8 @@ module.exports = {
     title: `Nishant Panchal`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  // plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
