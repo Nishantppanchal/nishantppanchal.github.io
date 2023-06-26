376cb8aea078167157573b090f98fa38aecfd7ad
Nishant Panchal <68797723+Nishantppanchal@users.noreply.github.com>
2023-06-27 02:45:44 +1000

    Research section

--- src/components/project.js
+++ src/components/project.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import Tag from './tag';
 
-function Project(props) {
+function Project({ projectName, date, tags, shortDes, longDes, links }) {
   return (
     <>
       <h1 className='flex flex-row mt-20 space-x-2 items-end text-xl'>
-        <div className='font-bold'>{props.projectName}</div>
-        <div className='text-slate-500'>{`- ${props.date}`}</div>
+        <div className='font-bold'>{projectName}</div>
+        <div className='text-slate-500'>{`- ${date}`}</div>
       </h1>
       <div className='flex flex-row overflow-auto mt-1 space-x-2'>
-        {props.tags.map((tag) => (
+        {tags.map((tag) => (
           <Tag tagText={tag} />
         ))}
       </div>
-      <p className='font-thin mt-3 text-lg'>{props.shortDes}</p>
+      <p className='font-thin mt-3 text-lg'>{shortDes}</p>
+      <div className='mt-3'>{longDes}</div>
       <div className='mt-3'>
-        {props.longDes}
-      </div>
-      <div className='mt-3'>
-        <span className='font-bold'>Links: </span>
-        {props.links.map(({ name, href }, i) => (
+        {links.length > 0 ? <span className='font-bold'>Links: </span> : null}
+        {links.map(({ name, href }, i) => (
           <span>
             <a href={href}>{name}</a>
-            {i < props.links.length - 1 ? ', ' : null}
+            {i < links.length - 1 ? ', ' : null}
           </span>
         ))}
       </div>
--- src/pages/research.js
+++ src/pages/research.js
@@ -1,15 +1,48 @@
 import * as React from 'react';
 import PageTemplate from '../components/pageTemplate';
 import Page from '../enums/pages';
+import Project from '../components/project';
 
-const NotFoundPage = () => {
+const research = [
+  {
+    projectName: 'Osteoporosis CT',
+    date: 'August 2022',
+    tags: [
+      'Artificial Intelligence',
+      'Deep Learning',
+      'Machine Learning',
+      'Radiology',
+      'Classification',
+      'PyTorch',
+    ],
+    shortDes: 'Deep learning algorithm to screen for Osteoporosis',
+    longDes: (
+      <p>
+        Developing a deep learning algorithm with PyTorch to screen for
+        Osteoporosis in patients based on head CT scans. For this research
+        project, I'm working on a team that is collaborating with Austin
+        Hospital to conduct the research.
+      </p>
+    ),
+    links: [],
+  },
+];
+
+function Research() {
   return (
     <PageTemplate page={Page.Research}>
-      <div className='text-3xl mt-20 text-center'>
-        🚧 Page Under Construction 🚧
-      </div>
+      {research.map(({ projectName, date, tags, shortDes, longDes, links }) => (
+        <Project
+          projectName={projectName}
+          date={date}
+          tags={tags}
+          shortDes={shortDes}
+          longDes={longDes}
+          links={links}
+        />
+      ))}
     </PageTemplate>
   );
-};
+}
 
-export default NotFoundPage;
+export default Research;
