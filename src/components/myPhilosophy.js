e94c286ebf9b05c51a773f397cf10f39fe03ff17
Nishant Panchal <68797723+Nishantppanchal@users.noreply.github.com>
2023-06-27 19:05:26 +1000

    Grammar fix

--- src/components/myPhilosophy.js
+++ src/components/myPhilosophy.js
@@ -23,8 +23,8 @@ function MyPhilosophy() {
       </h1>
       <p className='mt-5'>
         I believe you have to understand your code in it entirety to be able to
-        to create innovative software. Therefore I believe it is vital for us to
-        study Science of Computing to be able to master to the Art of
+        to create innovative software. Therefore, I believe it is vital for us
+        to study Science of Computing to be able to master to the Art of
         Programming. Hence, I have chosen to study Computer Science, from
         assembly code and Operating Systems to Massively Distributed Systems.
       </p>
